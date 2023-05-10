a0faeb97b9eb574209b6eca8e6034b295f6180dc
tzviki <twilhelm@votis.net>
2023-05-10 20:33:30 +0300

    123 אין לי באמת מה לכתוב עכשיו

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('server is running...');
+import express from 'express'
+const app = express()
+import dotenv from 'dotenv'
+dotenv.config()
+const port =  process.env.PORT || 5000
+
+import notFoundMiddleware from "./middleware/not-found.js"
+import errorHandlerMiddleware from './middleware/error-handler.js'
+
+app.get('/', (req, res) => {
+    // throw new Error('error')
+    res.send('Hello World!')
+})
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+app.listen(port, () => console.log(`Example app listening on port ${port}...!`))
